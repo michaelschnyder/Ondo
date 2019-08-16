b4bc72786cc75a755b5b124c6fc25b08ae7e57b8
RebekkaZ <rebekka.zahler@outlook.com>
2019-08-16 09:58:54 +0800

    update livingroom deviceid

--- cloud/src/data/ac.js
+++ cloud/src/data/ac.js
@@ -4,6 +4,5 @@ module.exports = Object.freeze({
     DINING_ROOM: new Device("Ondo-3c71bf3168b1", "DiningRoom"),
     MASTER_ROOM: new Device("Ondo-3c71bf316563", "MasterRoom"),
     GUEST_ROOM: new Device("Ondo-3c71bf316a40", "GuestRoom"),
-    //TODO: Define deviceID
-    LIVING_ROOM: new Device("Ondo-xyz123", "LivingRoom")
+    LIVING_ROOM: new Device("Ondo-3c71bf315b91", "LivingRoom")
 });
