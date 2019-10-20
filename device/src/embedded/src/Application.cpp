bd93f186b216573c1664442ae7a91579d2cceae6
Michael Schnyder <michael.schnyder@zuehlke.com>
2019-10-20 18:31:36 +0800

    fixed generation of id, when mac address contained leading zeros

--- device/src/embedded/src/Application.cpp
+++ device/src/embedded/src/Application.cpp
@@ -89,7 +89,7 @@ void Application::setGeneratedDeviceId() {
   byte mac[6]; 
   WiFi.macAddress(mac);
 
-  sprintf(deviceId, "Ondo-%2x%2x%2x%2x%2x%2x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  sprintf(deviceId, "Ondo-%02x%02x%02x%02x%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 }
 
 void Application::startupBanner() {
