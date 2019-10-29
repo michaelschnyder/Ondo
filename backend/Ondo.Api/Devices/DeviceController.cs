6373d3653f7c0b73231eb659f66215220a279c0b
Michael Schnyder <michael.schnyder@zuehlke.com>
2019-10-29 22:43:17 +0800

    list of devices with sensor data, location, wifi strenght and last update timestamps

--- backend/Ondo.Api/Devices/DeviceController.cs
+++ backend/Ondo.Api/Devices/DeviceController.cs
@@ -1,5 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Shared;
+using Microsoft.Extensions.Options;
 
 namespace Ondo.Api.Devices
 {
@@ -7,16 +13,54 @@ namespace Ondo.Api.Devices
     [ApiController]
     public class DeviceController : ControllerBase
     {
-        [HttpGet]
-        public IEnumerable<DeviceDto> Get()
+        private readonly AzureConfiguration _azureConfiguration;
+
+        public DeviceController(IOptions<AzureConfiguration> azureConfiguration)
         {
-            return new List<DeviceDto>();
+            _azureConfiguration = azureConfiguration.Value;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<DeviceDto>> Get()
+        {
+            RegistryManager registryManager = RegistryManager.CreateFromConnectionString(_azureConfiguration.IoTHubConnectionString);
+            var query = registryManager.CreateQuery("SELECT * FROM DEVICES");
+
+            var results = await query.GetNextAsTwinAsync();
+
+            return results.Select(Map);
         }
 
-        [HttpGet("{id}")]
-        public DeviceDto Get(int id)
+        private static DeviceDto Map(Twin twin)
         {
-            return new DeviceDto();
+            var deviceDto = new DeviceDto
+            {
+                DeviceId = twin.DeviceId,
+
+                ConnectionStatus = twin.ConnectionState.ToString(),
+                LastActivityTime = twin.LastActivityTime,
+                LastUpdateTime = twin.Properties.Reported.GetMetadata().LastUpdated
+
+
+            };
+            if (twin.Tags.Contains("location"))
+            {
+                deviceDto.Location = twin.Tags["location"].ToString();
+            }
+
+            if (twin.Properties.Reported.Contains("sensor"))
+            {
+                deviceDto.TemperatureC = float.Parse(twin.Properties.Reported["sensor"]["tempC"].ToString());
+                deviceDto.Humidity = float.Parse(twin.Properties.Reported["sensor"]["humidity"].ToString());
+            }
+
+            if (twin.Properties.Reported.Contains("network"))
+            {
+                deviceDto.Ip = twin.Properties.Reported["network"]["ip"].ToString();
+                deviceDto.Ssid = twin.Properties.Reported["network"]["ssid"].ToString();
+                deviceDto.Rssi = twin.Properties.Reported["network"]["rssi"].ToString();
+            }
+
+            return deviceDto;
         }
     }
 }
--- backend/Ondo.Api/Devices/DeviceDto.cs
+++ backend/Ondo.Api/Devices/DeviceDto.cs
@@ -1,6 +1,18 @@
-﻿namespace Ondo.Api.Devices
+﻿using System;
+
+namespace Ondo.Api.Devices
 {
     public class DeviceDto
     {
+        public string Ssid { get; set; }
+        public string DeviceId { get; set; }
+        public string Location { get; set; }
+        public string ConnectionStatus { get; set; }
+        public object TemperatureC { get; set; }
+        public object Humidity { get; set; }
+        public string Ip { get; set; }
+        public string Rssi { get; set; }
+        public DateTime? LastActivityTime { get; set; }
+        public DateTime LastUpdateTime { get; set; }
     }
 }
