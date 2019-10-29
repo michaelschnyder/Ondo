c0f3f4102d7b2ce2df9fa9221920c4fd32a8effa
Michael Schnyder <michael.schnyder@zuehlke.com>
2019-10-29 21:59:01 +0800

    added configuration and status api

--- backend/Ondo.Api/Startup.cs
+++ backend/Ondo.Api/Startup.cs
@@ -25,6 +25,8 @@ namespace Ondo.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<AzureConfiguration>(Configuration.GetSection("Azure"));
+
             services.AddControllers();
         }
 
--- backend/Ondo.Api/StatusController.cs
+++ backend/Ondo.Api/StatusController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Configuration;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace Ondo.Api
 {
@@ -6,10 +10,29 @@ namespace Ondo.Api
     [ApiController]
     public class StatusController : ControllerBase
     {
+        private readonly AzureConfiguration _azureConfiguration;
+
+        public StatusController(IOptions<AzureConfiguration> azureSettings)
+        {
+            _azureConfiguration = azureSettings.Value;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
+            if (string.IsNullOrWhiteSpace(_azureConfiguration.IoTHubConnectionString))
+            {
+                return this.Problem("Azure IoT Hub Connection String is empty", null,
+                    (int?) HttpStatusCode.InternalServerError, "Configuration Error", null);
+            }    
+
             return this.Ok();
         }
+
+    }
+
+    public class AzureConfiguration
+    {
+        public string IoTHubConnectionString { get; set; }
     }
 }
