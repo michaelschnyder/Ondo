0ef337bf71d7702e20d8cda0f101f29f5191f8d3
Michael Schnyder <michael.schnyder@zuehlke.com>
2020-01-13 19:17:57 +0800

    added new project that should serve as single entry for Mvc, webapi and other asp.net core based modules

---
+++ backend/Ondo.Backend/Startup.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Ondo.Api;
+using Ondo.Api.Services;
+using Ondo.Mvc.Controllers;
+
+namespace Ondo.Backend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var assembly = typeof(HomeController).Assembly;
+            services.AddControllersWithViews()
+                .AddApplicationPart(assembly);
+
+            services.Configure<AzureConfiguration>(Configuration.GetSection("Azure"));
+            services.AddSingleton<IDeviceService, DevicesService>();
+            services.AddSingleton<IAirConService, AirConService>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                env.WebRootFileProvider = new PhysicalFileProvider(Path.GetFullPath("..\\Ondo.Mvc\\wwwroot\\"));
+
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles(new StaticFileOptions() { });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=AirConOverview}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
