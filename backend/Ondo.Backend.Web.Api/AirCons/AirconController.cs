3cc164f5c819180d7678bc76df08c9f12566759c
RebekkaZ <rebekka.zahler@outlook.com>
2020-01-29 21:49:57 +0800

    Add AirconController to web api

---
+++ backend/Ondo.Backend.Web.Api/AirCons/AirconController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ondo.Backend.Core.Model;
+using Ondo.Backend.Core.Services;
+
+namespace Ondo.Api.Model
+{
+    [Route("api/aircon")]
+    [ApiController]
+    public class AirconController : ControllerBase
+    {
+        private readonly IAirConService _airConService;
+
+        public AirconController(IAirConService airConService)
+        {
+            _airConService = airConService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<AirCon>> Get()
+        {
+            var airCons = await _airConService.GetAirCons();
+            return airCons;
+        }
+
+        [HttpPost]
+        public async Task Post(AirCon airCon)
+        {
+            await _airConService.ChangeAirConSetting(airCon);
+        }
+    }
+}
