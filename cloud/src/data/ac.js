3f546181511e6721f1976353d876422a31c0a512
Michael Schnyder <michael.schnyder@zuehlke.com>
2020-04-19 15:40:27 +0800

    removed PoC Backend

--- cloud/src/data/ac.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = Object.freeze({
-    DINING_ROOM: { deviceId: "Ondo-3c71bf3168b1", location: "DiningRoom" },
-    MASTER_ROOM: { deviceId: "Ondo-3c71bf316563", location: "MasterRoom" },
-    GUEST_ROOM: { deviceId: "Ondo-3c71bf316a40", location: "GuestRoom" },
-    LIVING_ROOM: { deviceId: "Ondo-3c71bf315b91", location: "LivingRoom" }
-});
