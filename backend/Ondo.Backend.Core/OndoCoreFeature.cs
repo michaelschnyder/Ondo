a28325146fc280235f534f7e5913c6b2d5ef7eb1
RebekkaZ <rebekka.zahler@outlook.com>
2020-08-02 13:48:47 +0800

    change scheduler for home office

--- backend/Ondo.Backend.Core/OndoCoreFeature.cs
+++ backend/Ondo.Backend.Core/OndoCoreFeature.cs
@@ -31,38 +31,52 @@ namespace Ondo.Backend.Core
             //Everyday at 10pm
             const string RuleEverydayAt10pm = "0 0 22 ? * *";
 
-            //Weekend at 10am
-            const string RuleSaturdaysAt11am = "0 0 11 ? * SAT";
-            const string ruleSundaysAt11Am = "0 0 11 ? * SUN";
+            //Weekend at 11am
+            const string RuleWeekendAt11am = "0 0 11 ? * SAT-SUN";
+            
+            //Weekend at 8am
+            const string RuleWeekendAt8am = "0 0 11 ? * SAT-SUN";
 
             //Everyday at 2am
             const string RuleEverydayAt2am = "0 0 2 ? * *";
 
-            //Everyday at 6am
-            const string RuleEverydayAt6am = "0 0 6 ? * *";
+            //Everyday at 1am
+            const string RuleWorkday1am = "0 0 1 ? * MON-FRI";
 
             //Workdays at 9am
             const string RuleWorkdayAt9am = "0 0 9 ? * MON-FRI";
 
             //Workdays at 5pm
-            const string ruleWorkdayAt5pm = "0 0 17 ? * MON-FRI";
+            const string RuleWorkdayAt5pm = "0 0 17 ? * MON-FRI";
 
-            services.AddJob(GuestRoomAirconId, false, RuleEverydayAt10pm);
-            services.AddJob(GuestRoomAirconId, false, RuleWorkdayAt9am);
+            //Wordays at 7pm 
+            const string RuleWorkdayAt7pm = "0 0 19 ? * MON-FRI";
 
+            //Workdays at 12am
+            const string RuleWorkdayAt12am = "0 0 12 ? * MON-FRI";
+
+            //Workdays at 14am
+            const string RuleWorkdayAt14am = "0 0 14 ? * MON-FRI";
+
+            services.AddJob(GuestRoomAirconId, false, RuleWorkdayAt7pm);
+            services.AddJob(GuestRoomAirconId, true, RuleWorkdayAt9am);
+
+            services.AddJob(MasterRoomAirconId, true, RuleWorkdayAt9am);
             services.AddJob(MasterRoomAirconId, true, RuleEverydayAt10pm);
-            services.AddJob(MasterRoomAirconId, false, RuleSaturdaysAt11am);
-            services.AddJob(MasterRoomAirconId, false, ruleSundaysAt11Am);
-            services.AddJob(MasterRoomAirconId, false, RuleWorkdayAt9am);
-
+            services.AddJob(MasterRoomAirconId, false, RuleWeekendAt11am);
+            
             services.AddJob(DiningRoomAirconId, false, RuleEverydayAt10pm);
             services.AddJob(DiningRoomAirconId, false, RuleWorkdayAt9am);
 
             services.AddJob(LivingRoomAirconId, false, RuleEverydayAt2am);
-            services.AddJob(LivingRoomAirconId, true, RuleEverydayAt6am);
+            services.AddJob(LivingRoomAirconId, false, RuleWorkday1am);
+            services.AddJob(LivingRoomAirconId, true, RuleWeekendAt8am);
             services.AddJob(LivingRoomAirconId, false, RuleWorkdayAt9am);
-            services.AddJob(LivingRoomAirconId, true, ruleWorkdayAt5pm);
+            services.AddJob(LivingRoomAirconId, true, RuleWorkdayAt12am);
+            services.AddJob(LivingRoomAirconId, false, RuleWorkdayAt14am);
+            services.AddJob(LivingRoomAirconId, true, RuleWorkdayAt5pm);
             
+
             services.AddHostedService<QuartzHostedService>();
 
         }
